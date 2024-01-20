aa997aeeb249958c25b238481743fdd814900419
Matias Martinez <mat.martinezb@duocuc.cl>
2024-01-19 21:06:09 -0300

    Pagina en funcionamiento pero no lista los usuarios

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Alumno
 # Create your views here.
 
 def index(request):
+    alumnos= Alumno.objects.all()
     context={}
     return render(request,'alumnos/index.html',context)
+
